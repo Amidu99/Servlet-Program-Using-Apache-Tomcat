0263d483a3363a104f061158fce6b3e6091c83c6
Amidu99 <amidushamika@gmail.com>
2023-12-02 21:36:04 +0530

    finalize the HelloInitUser.java

--- task2/src/main/java/lk/ijse/task2/HelloInitUser.java
+++ task2/src/main/java/lk/ijse/task2/HelloInitUser.java
@@ -1,7 +1,21 @@
 package lk.ijse.task2;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class HelloInitUser extends HttpServlet {
+    @Override
+    public void init() throws ServletException {
+        System.out.println("init the HelloInitUser Object");
+    }
 
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("This is the HelloInitUser Class");
+        String username = getServletContext().getInitParameter("username");
+        System.out.println("Hello "+username+"!");
+    }
 }
