ceec5fda90b12e42487cef6777acacc8840dde0b
Amidu99 <amidushamika@gmail.com>
2023-12-02 21:30:00 +0530

    finalize the HelloUser.java

--- task2/src/main/java/lk/ijse/task2/HelloUser.java
+++ task2/src/main/java/lk/ijse/task2/HelloUser.java
@@ -1,7 +1,12 @@
 package lk.ijse.task2;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.*;
+import java.io.IOException;
 
 public class HelloUser extends HttpServlet {
-
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Hello User!");
+    }
 }
