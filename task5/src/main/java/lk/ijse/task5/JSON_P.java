dcfff23ccf662718b652f0ea528314f349a215bb
Amidu99 <amidushamika@gmail.com>
2023-12-15 18:24:58 +0530

    finalize JSON_P

---
+++ task5/src/main/java/lk/ijse/task5/JSON_P.java
@@ -0,0 +1,43 @@
+package lk.ijse.task5;
+
+import jakarta.json.Json;
+import jakarta.json.JsonArray;
+import jakarta.json.JsonObject;
+import jakarta.json.JsonReader;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name="jsonProcess", urlPatterns = "/jsonp")
+public class JSON_P extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if(req.getContentType()==null || !req.getContentType().toLowerCase().startsWith("application/json")) {
+            resp.sendError(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE);
+        }
+//        BufferedReader reader = req.getReader();
+//        StringBuilder sb = new StringBuilder();
+//        reader.lines().forEach(line-> sb.append(line+"\n"));
+//        System.out.println(sb);
+
+        JsonReader reader = Json.createReader(req.getReader());
+        JsonObject jsonObject = reader.readObject();
+
+        // object read
+        System.out.println("Name: "+jsonObject.getString("name"));
+        System.out.println("City: "+jsonObject.getString("city"));
+        System.out.println("Email: "+jsonObject.getString("email"));
+
+        // array read
+        JsonArray jsonValues = reader.readArray();
+        for (int i=0; i<jsonValues.size(); i++){
+            JsonObject jsonObject1 = jsonValues.getJsonObject(i);
+            System.out.println("Name: "+jsonObject.getString("name"));
+            System.out.println("City: "+jsonObject.getString("city"));
+            System.out.println("Email: "+jsonObject.getString("email"));
+        }
+    }
+}
