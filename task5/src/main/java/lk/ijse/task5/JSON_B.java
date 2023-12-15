6255475c6d55c7790382dd1fd41167178d7c75b3
Amidu99 <amidushamika@gmail.com>
2023-12-15 18:46:11 +0530

    finalize the task5

--- task5/src/main/java/lk/ijse/task5/JSON_B.java
+++ task5/src/main/java/lk/ijse/task5/JSON_B.java
@@ -1,7 +1,29 @@
 package lk.ijse.task5;
 
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
+import lk.ijse.task5.model.Student;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+@WebServlet(name="jsonBind", urlPatterns = "/jsonb")
 public class JSON_B extends HttpServlet {
-
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if (req.getContentType() == null || !req.getContentType().toLowerCase().startsWith("application/json")) {
+            resp.sendError(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE);
+        }
+        // json obj manipulation
+        Jsonb jsonb = JsonbBuilder.create();
+        List<Student> stuList = jsonb.fromJson(req.getReader(),
+                new ArrayList<Student>(){
+                }.getClass().getGenericSuperclass());
+        stuList.forEach(System.out::println);
+    }
 }
